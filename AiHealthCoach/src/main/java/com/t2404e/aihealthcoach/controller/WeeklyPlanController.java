b56eacf518118999aae150f8624983d1e6febf7b
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2026-01-18 23:01:25 +0700

    Add WeeklyAdjustmentRequest, WeeklyAdjustmentResponse, adjustWeeklyPlan in service, serviceImpl and controller

--- AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/controller/WeeklyPlanController.java
+++ AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/controller/WeeklyPlanController.java
@@ -1,9 +1,11 @@
 package com.t2404e.aihealthcoach.controller;
 
 import com.t2404e.aihealthcoach.common.ApiResponse;
+import com.t2404e.aihealthcoach.dto.request.WeeklyAdjustmentRequest;
 import com.t2404e.aihealthcoach.service.WeeklyPlanService;
 import com.t2404e.aihealthcoach.util.RequestUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -45,6 +47,23 @@ public class WeeklyPlanController {
                 ApiResponse.success(
                         "Weekly plans loaded",
                         service.getWeeklyPlans(monthlyPlanId, userId)
+                )
+        );
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/{weeklyPlanId}/adjust")
+    public ResponseEntity<ApiResponse<?>> adjust(
+            @PathVariable Long weeklyPlanId,
+            @Valid @RequestBody WeeklyAdjustmentRequest request,
+            HttpServletRequest httpRequest
+    ) {
+        Long userId = RequestUtil.getUserId(httpRequest);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        "Weekly plan adjusted",
+                        service.adjustWeeklyPlan(weeklyPlanId, userId, request)
                 )
         );
     }
