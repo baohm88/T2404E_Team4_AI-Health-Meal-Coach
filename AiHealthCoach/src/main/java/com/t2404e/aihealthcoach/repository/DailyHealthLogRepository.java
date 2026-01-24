11989849a5df94e9f3eeaf7681bf6586b64a3613
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2026-01-25 02:19:04 +0700

    Removed unnecessary entities & repos

--- AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/repository/DailyHealthLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.t2404e.aihealthcoach.repository;
-
-import com.t2404e.aihealthcoach.entity.DailyHealthLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyHealthLogRepository extends JpaRepository<DailyHealthLog, Long> {
-}
