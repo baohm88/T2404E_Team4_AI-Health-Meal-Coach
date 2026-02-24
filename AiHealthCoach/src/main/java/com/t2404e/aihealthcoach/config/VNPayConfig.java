9ee94d8c0747699c02b43e027e02c8e4ad32b9e7
TongM28 <tongm2802@gmail.com>
2026-02-24 15:25:39 +0700

    add new key

--- AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/config/VNPayConfig.java
+++ AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/config/VNPayConfig.java
@@ -15,8 +15,8 @@ public class VNPayConfig {
     // Bạn đăng ký tài khoản Sandbox VNPay để lấy các key này
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_ReturnUrl = "https://ai-healthcoach.site/api/payment/vnpay-return";
-    public static String vnp_TmnCode = "Z7SGOZ58"; // <--- THAY CÁI NÀY
-    public static String vnp_HashSecret = "YHNQDMSUCK63MK5EIXOLKBLDWYRBIH6N"; // <--- THAY CÁI NÀY
+    public static String vnp_TmnCode = "KJFOORY0"; // <--- THAY CÁI NÀY
+    public static String vnp_HashSecret = "DDZN9B5Y2GP8EMJDI81O3I1XGNA2CKG5"; // <--- THAY CÁI NÀY
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     // Hàm tiện ích: Hash SHA256 / SHA512
