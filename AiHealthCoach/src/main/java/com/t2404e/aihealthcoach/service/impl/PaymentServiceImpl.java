4fa05a61e9addcd0306cd47815af91ab769c0d97
TongM28 <tongm2802@gmail.com>
2026-02-25 00:20:19 +0700

    Update PaymentServiceImpl.java

--- AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/service/impl/PaymentServiceImpl.java
+++ AiHealthCoach/src/main/java/com/t2404e/aihealthcoach/service/impl/PaymentServiceImpl.java
@@ -60,15 +60,14 @@ public class PaymentServiceImpl implements PaymentService {
         vnp_Params.put("vnp_ReturnUrl", VNPayConfig.vnp_ReturnUrl);
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-        String vnp_CreateDate = formatter.format(cld.getTime());
+        ZoneId vnZone = ZoneId.of("Asia/Ho_Chi_Minh");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 
+        ZonedDateTime now = ZonedDateTime.now(vnZone);
+        String vnp_CreateDate = now.format(formatter);
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-        
 
-        cld.add(Calendar.MINUTE, 15);
-        String vnp_ExpireDate = formatter.format(cld.getTime());
+        String vnp_ExpireDate = now.plusMinutes(15).format(formatter);
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
         // Build Hash Data
